b5646ae652417c0d9353a2532516c51f6a4d9830
Vishal Sharma <66698503+VISHALSHARMA-CODE@users.noreply.github.com>
2022-05-21 00:55:25 -0700

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,17 @@
+
+import pygame
+
+WIDTH,HEIGHT= 600,600
+
+ROWS,COLS=8,8
+SQUARE_SIZE=WIDTH//COLS
+
+#rgb
+GREY= (255,0,0)
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+BLUE=(0,0,255)
+GREY=(128,128,128)
+
+
+CROWN = pygame.transform.scale(pygame.image.load('C:/Users/MJ/Downloads/crown.png'), (44, 25))
---
+++ main.py
@@ -0,0 +1,53 @@
+
+import pygame
+from checkers.constants import SQUARE_SIZE, WIDTH,HEIGHT
+from checkers.board import Board
+from checkers.piece import Piece
+from checkers.game import Game
+
+FPS=60
+WIN=pygame.display.set_mode((WIDTH,HEIGHT)) 
+pygame.display.set_caption('Checkers Game')
+
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row=y // SQUARE_SIZE
+    col= x // SQUARE_SIZE
+    return row,col
+
+def main():
+    run= True
+    clock=pygame.time.Clock()
+    game=Game(WIN)
+
+    # piece=board.get_piece(0,1)
+    # board.move(piece,4,3)
+
+    while run:
+        clock.tick(FPS)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run=False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos=pygame.mouse.get_pos()
+                row,col=get_row_col_from_mouse(pos)
+                # piece=board.get_piece(row,col)
+                # board.move(piece,4,3)
+                # if game.turn ==RED:
+                
+
+                game.select(row,col)
+
+
+        game.update ()        
+    pygame.quit()
+
+
+main()
